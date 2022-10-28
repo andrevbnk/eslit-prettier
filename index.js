de76eb65e078b1f8f0fa82afa6d49390514f8188
Andrew Ozzy <mor_ozzy@gplay.tech>
2022-10-28 23:59:35 +0300

    DEL: husky

--- index.js
+++
@@ -1,9 +0,0 @@
-export const handler = async (event) => {
-    const max = 999
-    const val = 0
-    const response = {
-        statusCode: 200,
-        body: `The random value (max ${max}) is: ${val}`,
-    }
-    return response
-}
